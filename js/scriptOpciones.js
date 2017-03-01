551112b2bcbbdeb448446e9e213ed26fcf683ac1
Belfor <alemandeleon86@gmail.com>
2017-03-01 20:57:29 +0000

    algunas modificaciones simples

--- js/scriptOpciones.js
+++ js/scriptOpciones.js
@@ -10,14 +10,16 @@ $(window).ready(function() {
 
             if (data.urls != undefined || data.urls != null) {
                 urls = data.urls;
+              $("#urls").append('<tr><th>Eliminar</th><th>URL</th></tr>');
 
                 for (i = 0; i < urls.length; i++) {
-                    $("#urls").append('<li class="list-group-item"> <span id="url' + i + '" class="glyphicon glyphicon-trash"></span> ' + data.urls[i] + "</li>");
+                    $("#urls").append('<tr> <td class=\"col-md-1\"><span id=\"url' + i +'\" class=\"glyphicon glyphicon-trash\"></span></th> <th class=\"col-md-10\">' + data.urls[i] + '</th></tr>');
+                  //  $("#urls").append('<li class="list-group-item"> <span id="url' + i + '" class="glyphicon glyphicon-trash"></span> ' + data.urls[i] + "</li>");
                     $("#url" + i).on("click", function() {
                         let i = $(this).attr("id").substr(3, $(this).attr("id").length);
                         var el = urls.splice(i, 1);
 
-                        $(this).closest('li').remove();
+                        $(this).closest('tr').remove();
                         chrome.storage.sync.set({
                             'urls': urls
                         });
@@ -27,11 +29,14 @@ $(window).ready(function() {
             if (data.frases != undefined || data.frases != null) {
                 frases = data.frases;
                 for (i = 0; i < frases.length; i++) {
-                    $("#frases").append('<li class="list-group-item"> <span id="frase' + i + '" class="glyphicon glyphicon-trash"></span> ' + data.frases[i] + "</li>");
+                    $("#frases").append('<tr> <td class="col-md-1"><span id="frase' + i +'" class="glyphicon glyphicon-trash"></span></th> <th class="col-md-10">' + data.frases[i] + "</th></tr>");
+              //      $("#frases").append('<li class="list-group-item"> <span id="frase' + i + '" class="glyphicon glyphicon-trash"></span> ' + data.frases[i] + "</li>");
                     $("#frase" + i).on("click", function() {
-                        let i = $(this).attr("id").substr(6, $(this).attr("id").length);
+                      console.log($(this).attr("id").length);
+                        let i = $(this).attr("id").substr(5, $(this).attr("id").length);
+                        console.log(i);
                         var el = frases.splice(i, 1);
-                        $(this).closest('li').remove();
+                        $(this).closest('tr').remove();
                         chrome.storage.sync.set({
                             'frases': frases
                         });
@@ -42,8 +47,9 @@ $(window).ready(function() {
     }
 
     function limpiar() {
-        $("#urls").empty();
-        $("#frases").empty();
+       $("#urls").empty();
+       $("#frases").empty();
+  
     }
 
     $("#insertURL").click(function() {
